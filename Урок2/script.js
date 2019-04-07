23c09c0bbbb15d895a2532866fc645709b88b5eb
Egorka <arhidemon@gmail.com>
2019-04-07 22:21:28 +0300

    lesson3

--- Урок2/script.js
+++ Урок2/script.js
@@ -24,3 +24,36 @@ switch(num) {
         console.log('nehyi')
 
 }
+
+
+
+
+while ( num < 55) {
+    console.log(num);
+    num++;
+}
+
+let n = 500
+
+do {
+    n++;
+    console.log(n);
+} while (n<550);
+
+for (let m = 1000; m < 10000; m++) {
+    console.log(m);
+    if (m == 5000) {
+        break;
+    }
+}
+
+
+let a = [1, 2, 3]; 
+let b = [1, 2, 3]; 
+if (a == b) {
+    console.log('true');
+} else {
+    console.log('false');
+}
+
+console.log(0 || "" || 2 || undefined || true || falsе);
---
+++ Урок2/tempCodeRunnerFile.js
@@ -0,0 +1,7 @@
+let a = [1, 2, 3]; 
+let b = [1, 2, 3]; 
+if (a == b) {
+    console.log('true');
+} else {
+    console.log('false');
+}
