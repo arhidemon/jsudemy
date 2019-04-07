e8316e632251047b48f6deb0e2c38c9a6371cafa
Egorka <arhidemon@gmail.com>
2019-04-08 00:48:33 +0300

    lll2

---
+++ Урок3/script.js
@@ -0,0 +1,34 @@
+
+
+
+
+
+
+// let calk = function(a,b){
+//     return a+b;
+// }
+
+let calk = (a,b) => a+b
+
+console.log(calk(3,4));
+
+
+function retVar() {
+    let num=50;
+    return num;
+}
+
+let anotherNum = retVar();
+console.log(anotherNum);
+
+let str='STring';
+
+console.log(str.length);
+console.log(str.toUpperCase())
+console.log(str.toLowerCase())
+
+let twelve = "12.2px";
+//console.log(Math.round(twelve);
+console.log(parseInt(twelve));
+
+console.log(parseFloat(twelve));
