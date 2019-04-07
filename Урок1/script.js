5e3b3b1d88f44734a3a5af982f91b28c0a978872
Egorka <arhidemon@gmail.com>
2019-04-07 15:17:47 +0300

    first

---
+++ Урок1/script.js
@@ -0,0 +1,25 @@
+'use strict';
+
+let a = 5;
+console.log(a);
+
+var number = 5;
+var string = 'Hello!';
+var sym = Symbol();
+var boolean = true;
+null;
+undefined;
+var obj = {};
+
+let persone = {
+    name: 'John',
+    age: 25,
+    isMarried: false
+};
+
+
+
+let isChecked = true;
+let isClose = false;
+
+console.log(!isChecked || isClose);
