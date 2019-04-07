5be0f2bbe5a1a4a7977621ac8410d8d73a1987ac
Egorka <arhidemon@gmail.com>
2019-04-07 17:46:49 +0300

    lesson2

---
+++ Урок2/script.js
@@ -0,0 +1,26 @@
+let num = 5
+
+if ( num < 4) {
+    console.log('menshe 4');
+} else if (num == 5) {
+    console.log('rovno5');
+} else {
+    console.log('hui znaet');
+}
+
+(num==5) ? console.log('yes') : console.log('noooo');
+
+switch(num) {
+    case num < 5:
+        console.log('1')
+        break;
+    case 5: 
+        console.log('2')
+        break;
+    case num > 5:
+        console.log('3');
+        break;
+    default:
+        console.log('nehyi')
+
+}
